628e3d94750d918c9b9feb6b7a1cb52cfdc364db
Greg Sharpe <me@gregsharpe.co.uk>
2019-02-12 14:09:18 +0000

    Removing flat file structure for the mgmt related terraform

--- terraform/01-bootstrap/main.tf
+++ terraform/01-bootstrap/main.tf
@@ -2,5 +2,5 @@ module "bootstrap" {
   source                  = "../modules/bootstrap"
   resource_group_location = "${var.resource_group_location}"
   resource_group_name     = "Web-Platform-Storage-Account"
-  storage_account = "webapplicationsa"
+  storage_account         = "webapplicationsa"
 }
--- terraform/bastion.public-ip.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_public_ip" "bastion_public_ip" {
-  name                = "loadbalancer-example-lb-public-ip"
-  location            = "${azurerm_resource_group.dog_application_resource_group.location}"
-  resource_group_name = "${azurerm_resource_group.dog_application_resource_group.name}"
-  allocation_method   = "Static"
-
-  tags = "${merge(var.instance_tags)}"
-}
